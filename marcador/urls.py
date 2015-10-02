fdde9575a5593fb8205ed8861c6a3947e72fef6f
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-02 17:21:08 +0300

    Exceptions tested and confirmed

--- marcador/urls.py
+++ marcador/urls.py
@@ -1,4 +1,4 @@
-"""marcador URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('marcador.urls')),
+    url(r'^', include('marcadorapp.urls')),
 ]
