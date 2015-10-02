abd943bf3c38d6ca808300e8796a3f4e3fc37ea8
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-02 15:48:01 +0300

    configaration settings added

--- marcador/settings.py
+++ marcador/settings.py
@@ -57,6 +57,7 @@ ROOT_URLCONF = 'marcador.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -102,3 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
