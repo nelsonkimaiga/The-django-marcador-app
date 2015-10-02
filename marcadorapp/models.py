06955bfb8e019a2dd4a01d77bbc51262d0bd52be
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-02 13:26:31 +0300

    Added models to models.py

--- marcadorapp/models.py
+++ marcadorapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Macardor(models.Model):
+	id=models.AutoField(primary_key=True)
+	url=models.models.URLField(max_length=20)
+	title=models.CharField(max_length=20)
+	description=models.TextField(max_length=20)
+	is_public=models.BooleanField(max_length=20)
+	date_created=models.DateTimeField(max_length=20)
+	date_updated=models.DateTimeField(max_length=20)
+	owner=models.ForeignKey(max_length=20)
+	tags=models.ManyToManyField(max_length=20)
+
+class Tag(models.Model):
+	id=models.AutoField(primary_key=True)
+	name=models.CharField(max_length=20)
